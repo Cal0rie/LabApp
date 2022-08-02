f406c94d5c3af0de71efb011fb682a0d862f98e5
Cal0rie <zmxianyu@163.com>
2022-08-02 16:41:58 +0800

    卸载无用插件

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@
 import {defineConfig} from 'vite'
 
 import uni from '@dcloudio/vite-plugin-uni'
-import requireTransform from 'vite-plugin-require-transform';
+//import requireTransform from 'vite-plugin-require-transform';
 
 //引入vant及相关插件
 //import Components from 'unplugin-vue-components/vite';
@@ -12,9 +12,9 @@ export default defineConfig({
 	// base: 'https://static-xxxx.bspapp.com/', // uniCloud 前端网页托管资源地址（主要是应用编译后的js，图片等静态资源，可以配置为二级目录）  
 	plugins: [
 		uni(),
-		requireTransform({
-		      fileRegex: /.js$|.vue$/
-		    }),
+		// requireTransform({
+		//       fileRegex: /.js$|.vue$/
+		//     }),
 		// Components({
 		//       resolvers: [VantResolver()],
 		//     }),
